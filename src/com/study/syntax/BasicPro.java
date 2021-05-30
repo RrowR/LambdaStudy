6c4ccbae01ae560a42764127572cd0132c023bfb
Mero <1043526080@qq.com>
2021-05-30 21:19:38 +0800

    lambda参数的方法体的精简以及返回值的精简

---
+++ src/com/study/syntax/BasicPro.java
@@ -0,0 +1,19 @@
+package com.study.syntax;
+
+import com.study.prepare.NoneReturnMultipleParameter;
+import com.study.prepare.NoneReturnNoneParameter;
+import com.study.prepare.SingleReturnSingleParameter;
+
+/**
+ * lambda表达式语法进阶
+ */
+public class BasicPro {
+    public static void main(String[] args) {
+        //1、实现一个参数，有返回值的函数式接口
+        SingleReturnSingleParameter lambda1 = a -> a * a;
+        System.out.println(lambda1.test(10));
+//        NoneReturnNoneParameter lambda2 = () -> {};
+//        NoneReturnMultipleParameter lambda3 = (a,b) -> System.out.println("a="+a+" b="+b);
+
+    }
+}
