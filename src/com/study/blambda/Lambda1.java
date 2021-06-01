793806117c5d4f44d23f9c11d8574247bd24eb2e
Mero <1043526080@qq.com>
2021-06-01 09:13:33 +0800

    lambda表达式的构造方法的学习，注意，类中的对象要是static的

--- src/com/study/blambda/Lambda1.java
+++ src/com/study/blambda/Lambda1.java
@@ -5,23 +5,34 @@ package com.study.blambda;
  */
 public class Lambda1 {
     private interface Calculate {
-        int calculate(int a,int b);
+        int calculate(int a, int b);
     }
 
     public static void main(String[] args) {
 //        Calculate calculate = (x,y) -> calculate(x,y);
 //        System.out.println(calculate.calculate(20,20));
         //Lambda1类引用calculate方法，也成为函数引用
-        Calculate calculate = Lambda1::calculate;
-        System.out.println(calculate.calculate(10,10));
+        //静态方法的引用
+//        Calculate calculate = Lambda1::calculate;
+//        System.out.println(calculate.calculate(10,10));
+        //非静态方法的引用，需要创建对象
+        Calculate calculate = new Lambda1()::calculate2;
+        System.out.println(calculate.calculate(1,2));
     }
 
-    private static int calculate(int x,int y){
-        if (x > y){
+    private int calculate2(int x,int y){
+        if(x!=y){
+            return x-y;
+        }
+        return x+y;
+    }
+
+    private static int calculate(int x, int y) {
+        if (x > y) {
             return x - y;
-        }else if (x < y){
+        } else if (x < y) {
             return y - x;
         }
-        return  x + y;
+        return x + y;
     }
 }
---
+++ src/com/study/blambda/Lambda2.java
@@ -0,0 +1,66 @@
+package com.study.blambda;
+
+public class Lambda2 {
+    public static class Person{
+
+        private String name;
+        private int age;
+
+        public Person() {
+            System.out.println("无参构造");
+        }
+
+        public Person(String name) {
+            this.name = name;
+            System.out.println("name的单个有参构造");
+        }
+
+        public Person(int age) {
+            this.age = age;
+            System.out.println("age的单个有参构造");
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+            System.out.println("name和age的有参构造");
+        }
+    }
+
+    @FunctionalInterface
+    public interface GetPerson{
+        Person get();
+    }
+
+    @FunctionalInterface
+    public interface GetOneParameterOfNamePerson{
+        Person get(String name);
+    }
+
+    @FunctionalInterface
+    public interface GetOneParameterOfAgePerson{
+        Person get(int age);
+    }
+
+    @FunctionalInterface
+    public interface GetMultipleParameterPerson{
+        Person get(String name,int age);
+    }
+
+    public static void main(String[] args) {
+        //这里一定要是无参的 静态 对象才可以
+        GetPerson getPerson = Person::new;
+        getPerson.get();
+
+        GetOneParameterOfNamePerson getOneParameterOfNamePerson = Person::new;
+        getOneParameterOfNamePerson.get("镜嬅");
+
+        GetOneParameterOfAgePerson getOneParameterOfAgePerson = Person::new;
+        getOneParameterOfAgePerson.get(23);
+
+        GetMultipleParameterPerson getMultipleParameterPerson = Person::new;
+        getMultipleParameterPerson.get("美美",12);
+
+    }
+
+}
